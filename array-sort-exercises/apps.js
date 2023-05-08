4f32c7d1cb8e3e1c4c2413eb7e20252ce7808fdd
Takeshi Garcia <garcia.takeshi@yahoo.com>
2023-05-08 12:32:57 -0700

    assignments

--- array-sort-exercises/apps.js
+++ array-sort-exercises/apps.js
@@ -1,32 +1,32 @@
 function leastToGreatest(arr) {
-    test = arr.sort(function(a , b){
+    nums = arr.sort(function(a , b){
         return a-b
     })
-    return test
+    return nums
   }
   
   console.log(leastToGreatest([1, 3, 5, 2, 90, 20])); // [1, 2, 3, 5, 20, 90]
 
 function greatestToLeast(arr) {
-    test = arr.sort(function(a , b){
+    nums = arr.sort(function(a , b){
                 return b - a
             })
-            return test
+            return nums
   }
   
   console.log(greatestToLeast([1, 3, 5, 2, 90, 20])); // [90, 20, 5, 3, 2, 1]
 
 function lengthSort(arr) {
-    test = arr.sort(function(a , b){
+    leng = arr.sort(function(a , b){
         return a.length - b.length
     })
-    return test
+    return leng
   }
   
   console.log(lengthSort(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "wolf", "eaten", "family"]
   
 function alphabetical(arr) {
-    test = arr.sort(function(a , b){
+    leng = arr.sort(function(a , b){
         if (a < b) {
             return -1;
           }
@@ -36,17 +36,17 @@ function alphabetical(arr) {
           return 0;
 
     })
-    return test
+    return leng
 }
 
 console.log(alphabetical(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "eaten", "family", "wolf"]
 
 
 function byAge(arr){
-    test = arr.sort(function(a , b){
+    newA = arr.sort(function(a , b){
         return a.age - b. age
     })
-    return test
+    return newA
   }
   
   console.log(byAge([
