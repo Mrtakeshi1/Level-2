190a7ad70837a029f40497db9fc7db4380dd81c7
Takeshi Garcia <garcia.takeshi@yahoo.com>
2023-08-21 13:12:45 -0700

    update

--- array-sort-exercises/apps.js
+++ array-sort-exercises/apps.js
@@ -1,8 +1,6 @@
 function leastToGreatest(arr) {
-    nums = arr.sort(function(a , b){
-        return a-b
-    })
-    return nums
+    return arr.sort((a , b) => a - b)
+  
   }
   
   console.log(leastToGreatest([1, 3, 5, 2, 90, 20])); // [1, 2, 3, 5, 20, 90]
@@ -17,26 +15,28 @@ function greatestToLeast(arr) {
   console.log(greatestToLeast([1, 3, 5, 2, 90, 20])); // [90, 20, 5, 3, 2, 1]
 
 function lengthSort(arr) {
-    leng = arr.sort(function(a , b){
-        return a.length - b.length
-    })
-    return leng
+    return arr.sort(a , b => a.length - b.length)
   }
   
   console.log(lengthSort(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "wolf", "eaten", "family"]
+  // stringArray.sort((word1, word2) => (word1 < word2) ? -1 : (word1 > word2) ? 1 : 0);
   
-function alphabetical(arr) {
-    leng = arr.sort(function(a , b){
-        if (a < b) {
-            return -1;
-          }
-          if (a > b) {
-            return 1;
-          }
-          return 0;
+function alphabetical(arr) {// check for 1     //Check for 0      check for -1
+                              //a is after b  //Nothing happens   //b is after a
+   return arr.sort((a , b) => a > b ? 1 :      a === b ? 0          : -1);
+// sort will assign position based on numerical result
 
-    })
-    return leng
+   //    leng = arr.sort(function(a , b){ 
+//     if (a < b) {
+//         return -1;
+//       }
+//       if (a > b) {
+//         return 1;
+//       }
+//       return 0;
+
+// })
+// return leng
 }
 
 console.log(alphabetical(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "eaten", "family", "wolf"]
